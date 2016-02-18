48aafb412bb8ddf63768ae0915a6540af27c1eb9
Andrés Ortiz <ortizvillalba@gmail.com>
2016-02-18 10:03:37 -0300

    se pasa el TemplateV2 version 5.7.5.2 al branch v5.7.5.2

--- application/blocks/blocks/generarcalendario/edit.php
+++
@@ -1,9 +0,0 @@
-<!--
-	Código al que se llama cuando se edita un bloque existente
--->
-<?php 
-defined('C5_EXECUTE') or die("Access Denied.");
-$bObj=$controller;
-?>
-
-<?php $this->inc('form_setup_html.php', array('bObj' => $bObj)); ?> 
--- application/blocks/boton/composer.php
+++
@@ -1,9 +0,0 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");
-$form = Loader::helper('form');
-$html = Loader::helper('html');
-$url = Loader::helper('concrete/urls');
-
-$setcontrol = $control->getPageTypeComposerFormLayoutSetControlObject();
-$al = Loader::helper('concrete/asset_library');
-
-?>
