2b98d7e310ba4a7e83ecf99b11c6472f5775b27d
Andres Ortiz <ortizvillalba@gmail.com>
2016-05-02 09:17:36 -0400

    Se actualiza el el gitignore

---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/2ce5bb63729f652e/46009478f58ccbdb.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_2214ee6fab68af65c920d7facd6e0403_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1462612401;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1462189128;
